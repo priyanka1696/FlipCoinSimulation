db044c0ca378b99d4ed3a530256d18553a30bcb1
Priyanka <fulmogarepriya@gmail.com>
2020-05-15 12:07:19 +0530

    Multiple Times Of Flipping Coin And Show Number Of Head And Tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,11 +2,22 @@
 echo "Welcome To Flip Coin Simulation "
 
 IsHead=0;
-randomCheck=$(( RANDOM%2 ))
+IsHeadWon=0;
+IsTailWon=0;
+
+for (( count=0; count<=10; count++ ))
+do
+	randomCheck=$(( RANDOM%2 ))
 
 if [[ IsHead -eq randomCheck ]]
 then
-	echo "Heads"
+	echo "Head"
+	IsHeadWon=$(( $IsHeadWon + 1 ))
 else
-	echo "Tails"
+	echo "Tail"
+	IsTailWon=$(( $IsTailWon + 1 ))
 fi
+done
+
+echo "Total Number Of Head Win Is $IsHeadWon"
+echo "Total Number Of Tail Win Is $IsTailWon"
