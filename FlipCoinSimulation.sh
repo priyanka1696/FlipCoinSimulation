5fe841bb334a14410e2765009e9f408c4a3dad86
Priyanka <fulmogarepriya@gmail.com>
2020-05-15 12:19:09 +0530

    Show If It's A Win Or Tie If Win Then Who Won And By How Much

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,19 +5,29 @@ IsHead=0;
 IsHeadWon=0;
 IsTailWon=0;
 
-for (( count=0; count<=10; count++ ))
+while [[ $IsHeadWon -lt 21 && $IsTailWon -lt 21 ]]
 do
 	randomCheck=$(( RANDOM%2 ))
 
 if [[ IsHead -eq randomCheck ]]
 then
-	echo "Head"
+	echo "Tail"
 	IsHeadWon=$(( $IsHeadWon + 1 ))
 else
-	echo "Tail"
+	echo "Head"
 	IsTailWon=$(( $IsTailWon + 1 ))
 fi
 done
 
 echo "Total Number Of Head Win Is $IsHeadWon"
 echo "Total Number Of Tail Win Is $IsTailWon"
+
+if [[ IsHeadWon -lt IsTailWon ]]
+then
+	echo "Tail Win"
+elif [[ IsHeadWon -eq IsTailWon ]]
+then
+	echo "Tie"
+else
+	echo "Head Win"
+fi
